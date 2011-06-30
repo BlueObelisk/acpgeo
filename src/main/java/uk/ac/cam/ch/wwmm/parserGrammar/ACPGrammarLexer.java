76f90ad71afdd75c28e455e7b58707f8bd80c8f2
lh359 <lh359@cam.ac.uk>
2011-06-30 15:10:06 +0100

    unit tests should work now

--- src/main/java/uk/ac/cam/ch/wwmm/parserGrammar/ACPGrammarLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/parserGrammar/ACPGrammarLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\parserGrammar\\ACPGrammar.g 2011-06-30 14:37:34
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\parserGrammar\\ACPGrammar.g 2011-06-30 15:06:44
 package uk.ac.cam.ch.wwmm.parserGrammar;
 
 import org.antlr.runtime.*;
